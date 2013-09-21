ee622e10e4639ea45da7ee6ac1b21dfb8ce97167
unknown <lafengmaker@gmail.com>
2013-09-21 20:33:19 +0800

    first init

---
+++ Activity_02/gen/com/lafeng/activity_01/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.lafeng.activity_01;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Activity_02/src/com/lafeng/activity_01/ActivityOther.java
@@ -0,0 +1,14 @@
+package com.lafeng.activity_01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityOther extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.other);
+	}
+	
+}
